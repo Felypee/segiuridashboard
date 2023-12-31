908b2c42315463e7fab8e6e7b1c1f7465af972be
Felipe Arango <felipearango9701@gmail.com>
2023-12-31 14:46:36 -0500

    add new facebook button logic 14

--- src/utils/FacebookSdk.jsx
+++ src/utils/FacebookSdk.jsx
@@ -11,60 +11,37 @@ import { useEffect, useState } from "react";
 export const FacebookBButton = () => {
   // const [isLoggedin, setIsLoggedin] = useState(false);
 
-  const onLoginClick = async() =>{
-    const http = new XMLHttpRequest();
-const clientId = '652064637073116';  // Reemplaza con tu ID de aplicación de Facebook
-const redirectUri = 'https://segiuri.netlify.app';
+  const onLoginClick = async() =>
+    await window.FB.login(
+      function (response) {
+        console.log(response);
+      },
+      {
+        config_id: "6854044498033268",
+      }
+    );
 
-// Construir la URL de inicio de sesión de Facebook
-const facebookLoginUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${clientId}`;
-
-// Configurar la solicitud
-http.open('GET', facebookLoginUrl, true);
-
-// Manejar la respuesta
-http.onreadystatechange = function () {
-  if (http.readyState === 4 && http.status === 200) {
-    console.log(http.responseText);
-    // Aquí puedes manejar la respuesta, por ejemplo, redirigir a la URL de Facebook
-  }
-};
-
-// Enviar la solicitud
-http.send();
-
-  }
-  
-  //   await window.FB.login(
-  //     function (response) {
-  //       console.log(response);
-  //     },
-  //     {
-  //       config_id: "233305346384438",
-  //     }
-  //   );
-
-  // useEffect(() => {
-  //   window.fbAsyncInit = () => {
-  //     window.FB.init({
-  //       appId: "652064637073116",
-  //       cookie: true,
-  //       xfbml: true,
-  //       version: "v18.0",
-  //     });
-  //   };
-  //   (function (d, s, id) {
-  //     var js,
-  //       fjs = d.getElementsByTagName(s)[0];
-  //     if (d.getElementById(id)) {
-  //       return;
-  //     }
-  //     js = d.createElement(s);
-  //     js.id = id;
-  //     js.src = "https://connect.facebook.net/en_US/sdk.js";
-  //     fjs.parentNode.insertBefore(js, fjs);
-  //   })(document, "script", "facebook-jssdk");
-  // }, []);
+  useEffect(() => {
+    window.fbAsyncInit = () => {
+      window.FB.init({
+        appId: "652064637073116",
+        cookie: true,
+        xfbml: true,
+        version: "v18.0",
+      });
+    };
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }, []);
 
   return (
     <div>
