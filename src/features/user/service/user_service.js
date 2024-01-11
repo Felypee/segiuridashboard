72eea0c1e8e2e914269d18f9486fc83fd82d5ee6
Felipe Arango <felipearango9701@gmail.com>
2024-01-11 14:01:48 -0500

    add endpoint to save page info 4

--- src/features/user/service/user_service.js
+++ src/features/user/service/user_service.js
@@ -5,7 +5,7 @@ const metaApi = "https://graph.facebook.com/v18.0/";
 export const getMetaUserPages = async (accessToken, userId) => {
   try {
     const response = await axios.get(
-      `${metaApi}/userId/accounts?access_token=${accessToken}`
+      `${metaApi}/${userId}/accounts?access_token=${accessToken}`
     );
     console.log("getMetaUserPages Response:  ", response.data.data[0]);
 
