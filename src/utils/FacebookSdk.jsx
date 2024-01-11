c6005a38471c5b060eaf79be2cd13d699fc135dc
Felipe Arango <felipearango9701@gmail.com>
2024-01-11 16:16:11 -0500

    add endpoint to save page info 7

--- src/utils/FacebookSdk.jsx
+++ src/utils/FacebookSdk.jsx
@@ -24,7 +24,7 @@ export const FacebookBButton = () => {
         setUser(response.authResponse)
       },
       {
-        config_id: "1353145698650888",
+        config_id: "399416789315523",
       }
     );
 
