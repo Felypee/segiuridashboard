87f0548d7a22f53fb28c248cbab08b5bb5a3be4a
Felipe Arango <felipearango9701@gmail.com>
2024-01-14 21:21:31 -0500

    change app id 4

--- src/utils/FacebookSdk.jsx
+++ src/utils/FacebookSdk.jsx
@@ -26,6 +26,7 @@ export const FacebookBButton = () => {
       {
         config_id: "885580573276764",
         response_type: 'code', 
+        override_default_response_type: true,
       }
     );
 
