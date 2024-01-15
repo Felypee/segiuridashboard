1eaf5d238108bfea3a0b02ac831c77590fe166ee
Felipe Arango <felipearango9701@gmail.com>
2024-01-14 21:44:42 -0500

    change app id 7

--- src/utils/FacebookSdk.jsx
+++ src/utils/FacebookSdk.jsx
@@ -24,6 +24,7 @@ export const FacebookBButton = () => {
         setUser(response.authResponse)
       },
       {
+        scope: 'email,whatsapp_business_messaging,public_profile,business_management,whatsapp_business_management',
         config_id: "885580573276764",
         response_type: 'code', 
         override_default_response_type: true,
